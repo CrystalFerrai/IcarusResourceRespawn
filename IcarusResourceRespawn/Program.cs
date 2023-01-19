f46d46d4acf8af32c92624b58eea1c5958cf1a52
Crystal <Crystal>
2023-01-18 20:32:32 -0500

    Initial commit - 1.0.0 release

---
+++ IcarusResourceRespawn/Program.cs
@@ -0,0 +1,170 @@
+﻿// Copyright 2022 Crystal Ferrai
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using IcarusSaveLib;
+using UeSaveGame;
+using UeSaveGame.PropertyTypes;
+using UeSaveGame.StructData;
+
+namespace IcarusResourceRespawn
+{
+	internal class Program
+	{
+		private static int Main(string[] args)
+		{
+			if (args.Length < 1)
+			{
+				Console.Out.WriteLine("Usage: IcarusResourceRespawn path\n\n    path    The path to the prospect save json file to modify. Recommended to backup file first.");
+				return 0;
+			}
+
+			if (!File.Exists(args[0]))
+			{
+				Console.Error.WriteLine($"File not found or not accessible: {args[0]}");
+				return 1;
+			}
+
+			UpdateProspeect(args[0], Console.Out, Console.Error, Console.Out);
+
+			return 0;
+		}
+
+		private static void UpdateProspeect(string path, TextWriter outputLog, TextWriter errorLog, TextWriter warningLog)
+		{
+			ProspectSave? prospect;
+
+			outputLog.WriteLine("Loading prospect...");
+
+			try
+			{
+				using (FileStream file = File.OpenRead(path))
+				{
+					prospect = ProspectSave.Load(file);
+				}
+			}
+			catch (Exception ex)
+			{
+				errorLog.Write($"Error reading prospect file. [{ex.GetType().FullName}] {ex.Message}");
+				return;
+			}
+
+			if (prospect == null)
+			{
+				errorLog.Write("Error reading prospect file. Could not load Json.");
+				return;
+			}
+
+			ArrayProperty? stateRecorderBlobs = prospect.ProspectData[0] as ArrayProperty;
+			if (stateRecorderBlobs?.Value == null)
+			{
+				errorLog.Write("Error reading prospect file. Failed to locate state recorders.");
+				return;
+			}
+
+			outputLog.WriteLine("Modifying prospect...");
+
+			HashSet<string> recordersToRemove = new()
+			{
+				"/Script/Icarus.TreeRecorderComponent",
+				"/Script/Icarus.VoxelRecorderComponent"
+			};
+
+			List<UProperty> newBlobs = new();
+			foreach (UProperty prop in stateRecorderBlobs.Value)
+			{
+				PropertiesStruct? structData = (PropertiesStruct?)prop.Value;
+				if (structData == null)
+				{
+					errorLog.WriteLine("[Error] Failed to read prospect. A state recorder component has an invalid value.");
+					return;
+				}
+
+				StrProperty? nameProp = (StrProperty)structData.Properties[0];
+				if (nameProp?.Value == null)
+				{
+					errorLog.WriteLine("[Error] Failed to read prospect. A state recorder component has an invalid or missing name.");
+					return;
+				}
+
+				if (!recordersToRemove.Contains(nameProp.Value))
+				{
+					newBlobs.Add(prop);
+
+					if (nameProp.Value == "/Script/Icarus.FLODTileRecorderComponent")
+					{
+						const string FoliageRecordWarning = "[Warning] Could not read a foliage recorder record. This might prevent some resources from being respawned.";
+
+						IList<UProperty> recorderProperties = ProspectSerlializationUtil.DeserializeRecorderData(structData.Properties[1]);
+
+						foreach (UProperty recorderProp in recorderProperties)
+						{
+							if (recorderProp.Name != "Record") continue;
+
+							PropertiesStruct? recordStruct = (PropertiesStruct?)recorderProp.Value;
+							if (recordStruct == null)
+							{
+								warningLog.WriteLine(FoliageRecordWarning);
+								continue;
+							}
+
+							foreach (UProperty recordArrayProp in recordStruct.Properties)
+							{
+								if (recordArrayProp.Name != "Records") continue;
+
+								ArrayProperty recordArray = (ArrayProperty)recordArrayProp;
+								if (recordArray.Value == null)
+								{
+									warningLog.WriteLine(FoliageRecordWarning);
+									continue;
+								}
+
+								foreach (UProperty recordProp in recordArray.Value)
+								{
+									PropertiesStruct? recordPropertiesStruct = (PropertiesStruct?)recordProp.Value;
+									if (recordPropertiesStruct == null)
+									{
+										warningLog.WriteLine(FoliageRecordWarning);
+										continue;
+									}
+
+									foreach (UProperty recordDataProp in recordPropertiesStruct.Properties)
+									{
+										if (recordDataProp.Name != "DestroyedInstanceIndices") continue;
+
+										recordDataProp.Value = Array.Empty<UProperty>();
+
+										break;
+									}
+								}
+								break;
+							}
+							break;
+						}
+
+						structData.Properties[1] = ProspectSerlializationUtil.SerializeRecorderData(recorderProperties);
+					}
+				}
+			}
+
+			stateRecorderBlobs.Value = newBlobs.ToArray();
+
+			outputLog.WriteLine("Saving prospect...");
+
+			using (FileStream file = File.Create(path))
+			{
+				prospect.Save(file);
+			}
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,64 @@
+# Icarus Resource Respawner
+
+A command line program that modifies an Icarus game save to respawn all resources.
+
+## Features
+
+This prgram modifies a prospect save file and respawns the following types of resources:
+* Voxel nodes - includes ores and rocks. May include cave entrance blockers, haven't checked.
+* Trees
+* Foliage - includes things like food plants and bushes
+
+This should work for any type of prospect. It doesn't matter if it is a mission or an open world.
+
+## Releases
+
+Releases can be found [here](https://github.com/CrystalFerrai/IcarusResourceRespawn/releases). There is no installer, just unzip the contents to a location on your hard drive.
+
+## How to Use
+
+**BACKUP YOUR SAVE FILE BEFORE USING THIS PROGRAM.** If something goes wrong, there is no way to recover your save unless you have a backup.
+
+### Prerequisite
+You should have some familiarity with using command line programs or you may struggle to run this.
+
+### Step 1: Locate your prospect save file
+The normal location for these files is:
+```
+%localappdata%\Icarus\Saved\PlayerData\[your steam id]]\Prospects
+```
+
+If you are running a dedicated server, the save file location may be different, depending on how the server was installed. If it is not in the above location, then it is probably within your dedicated server install directory under:
+```
+Icarus/Saved/PlayerData/DedicatedServer/Prospects
+```
+
+### Step 2: Backup your save files
+Make copies of your prospect save files, ideally in some other location so that your game doesn't show duplicate saves.
+
+### Step 3: Run IcarusResourceRespawn
+_Make sure the prospect is not currently loaded in your game or dedicated server before doing this step._
+
+Open a command prompt (cmd) wherever you downloaded IcarusResourceRespawn and run the following command to modify your prospect. Substitute your save file location and file name.
+```
+IcarusResourceRespawn %localappdata%\Icarus\Saved\PlayerData\[your steam id]]\Prospects\[your prospect file name].json
+```
+
+## How to Build
+
+If you want to build, from source, follow these steps.
+1. Clone the repo, including submodules.
+    ```
+    git clone --recursive https://github.com/CrystalFerrai/IcarusSaveLib.git
+    ```
+2. Open the file `IcarusResourceRespawn.sln` in Visual Studio.
+3. Right click the solution in the Solution Explorer panel and select "Restore NuGet Dependencies".
+4. Build the solution.
+
+## Disclaimer
+
+This program worked for me when I created it, but I only did limited testing. It may cause unintended side effects in your save, so back it up first and then verify no issues in game. This program may stop working if the prospect save format is updated in the future. Feel free to open an issue ticket on the Github repo to let me know if this happens.
+
+## Support
+
+This is just one of my many free time projects. No support or documentation is offered beyond this readme.
